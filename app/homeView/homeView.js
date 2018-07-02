b7463fd6becae108582a3900195539281e9fea71
Simone Aiken <saiken@ulfheim.net>
2018-07-02 09:06:54 -0600

    Naming views properly refactor 2

--- app/homeView/homeView.js
+++ app/homeView/homeView.js
@@ -1,14 +1,14 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.homeView', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+  $routeProvider.when('/homeView', {
+    templateUrl: 'homeView/homeView.html',
+    controller: 'HomeViewCtrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('HomeViewCtrl', [function() {
 
 }]);
